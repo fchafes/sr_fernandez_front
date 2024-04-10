191d0732d43d4f8d271bcf634f1bd4d47df68fee
fchafes <64428168+fchafes@users.noreply.github.com>
2024-04-10 16:51:52 -0300

    mobile home gif

--- src/components/mobile/MobileHome.jsx
+++ src/components/mobile/MobileHome.jsx
@@ -14,31 +14,40 @@ function MobileHome() {
       <div className="m-home-container">
         <div className="top-media">
           <div className="header-container">
-            <img src="src/assets/mobile_home.gif" alt="" />
+            <img
+              src="https://wbnzdyxchqficjywllpl.supabase.co/storage/v1/object/public/mi%20primer%20bucket/mobile/gif/mobile_home.gif"
+              alt=""
+            />
           </div>
         </div>
 
         <div className="bottom-media">
           <div className="accordion">
             {workData.map((index) => {
-              return(
-              <div
-                key={index}
-                className="tab-container"
-                style={ { height: `${activeTab === index ? '70px' : ''}` } }
-                onClick={() => tabToggle(index)}
-              >
-                <div className="tab">
-                  <div className="tab-left">
-                    <img src={index.images[0]} alt="" />
-                  </div>
-                  <div className="tab-right"
-                  style={{ backgroundColor: `${activeTab === index ? 'yellow' : ''}` }}>
-                    <h3>{index.title}</h3>
+              return (
+                <div
+                  key={index}
+                  className="tab-container"
+                  style={{ height: `${activeTab === index ? "70px" : ""}` }}
+                  onClick={() => tabToggle(index)}
+                >
+                  <div className="tab">
+                    <div className="tab-left">
+                      <img src={index.images[0]} alt="" />
+                    </div>
+                    <div
+                      className="tab-right"
+                      style={{
+                        backgroundColor: `${
+                          activeTab === index ? "yellow" : ""
+                        }`,
+                      }}
+                    >
+                      <h3>{index.title}</h3>
+                    </div>
                   </div>
                 </div>
-              </div>
-              )
+              );
             })}
           </div>
         </div>
