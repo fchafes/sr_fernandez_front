d0a0c798eacd27784626ea50277d482b5a0918d8
fchafes <64428168+fchafes@users.noreply.github.com>
2024-04-15 01:23:00 -0300

    about

--- src/pages/MobileAbout.jsx
+++ src/pages/MobileAbout.jsx
@@ -2,6 +2,8 @@ import "./MobileAbout.css";
 import MobileNavbar from "../components/mobile/MobileNavbar";
 
 function MobileAbout() {
+  const bucket =
+    "https://wbnzdyxchqficjywllpl.supabase.co/storage/v1/object/public/mi%20primer%20bucket";
   return (
     <>
       <MobileNavbar />
@@ -16,6 +18,11 @@ function MobileAbout() {
           <h4>T</h4>
           <h4>!</h4>
         </div>
+        {/* <div className="m-big-about-behind">
+          <h4>A</h4>
+          <h4>B</h4>
+          <img src={`${bucket}/gif/about.gif`} alt="" />
+        </div> */}
       </div>
     </>
   );
