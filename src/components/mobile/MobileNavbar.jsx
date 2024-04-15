342084092defd8f7ed8a1658ba98b4e338a599c1
fchafes <64428168+fchafes@users.noreply.github.com>
2024-04-14 23:07:47 -0300

    close button - mobile menu

--- src/components/mobile/MobileNavbar.jsx
+++ src/components/mobile/MobileNavbar.jsx
@@ -17,7 +17,10 @@ function MobileNavbar({ handleGlitch }) {
         <Link to="#" className="mobile-navbar-logo" onClick={handleClick}>
           Sr. Fernandez
         </Link>
-        <p onClick={handleMenu}>Menu</p>
+        <div onClick={handleMenu} className="menu-button">
+          <div className={`line-1 ${isMenuOpen ? "line-1-x" : ""}`}></div>
+          <div className={`line-2 ${isMenuOpen ? "line-2-x" : ""}`}></div>
+        </div>
       </nav>
       <div className={`open-menu ${isMenuOpen ? "show-open-menu" : ""}`}>
         <ul>
@@ -31,12 +34,12 @@ function MobileNavbar({ handleGlitch }) {
             <Link>contact</Link>
           </li>
           <li>
-          <Link>
-            <img
-              src="https://wbnzdyxchqficjywllpl.supabase.co/storage/v1/object/public/mi%20primer%20bucket/images/sr_iso_white.png"
-              alt="isologo_elephant"
-            />
-          </Link>
+            <Link>
+              <img
+                src="https://wbnzdyxchqficjywllpl.supabase.co/storage/v1/object/public/mi%20primer%20bucket/images/sr_iso_white.png"
+                alt="isologo_elephant"
+              />
+            </Link>
           </li>
         </ul>
       </div>
