3a55ee52a4f74ea1817a4aa3eb640fd7657e9538
fchafes <64428168+fchafes@users.noreply.github.com>
2024-04-14 22:04:26 -0300

    menu open

--- src/components/mobile/MobileNavbar.jsx
+++ src/components/mobile/MobileNavbar.jsx
@@ -1,9 +1,14 @@
 import "./MobileNavbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function MobileNavbar({ handleGlitch }) {
   const handleClick = () => {
     handleGlitch();
+  };
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
@@ -12,9 +17,9 @@ function MobileNavbar({ handleGlitch }) {
         <Link to="#" className="mobile-navbar-logo" onClick={handleClick}>
           Sr. Fernandez
         </Link>
-        <p>Menu</p>
+        <p onClick={handleMenu}>Menu</p>
       </nav>
-      <div className="open-menu">
+      <div className={`open-menu ${isMenuOpen ? "show-open-menu" : ""}`}>
         <ul>
           <li>
             <Link>works</Link>
@@ -28,7 +33,10 @@ function MobileNavbar({ handleGlitch }) {
         </ul>
         <div className="mobile-menu-footer">
           <Link>
-            <img src="https://wbnzdyxchqficjywllpl.supabase.co/storage/v1/object/public/mi%20primer%20bucket/images/sr_iso_white.png" alt="isologo_elephant" />
+            <img
+              src="https://wbnzdyxchqficjywllpl.supabase.co/storage/v1/object/public/mi%20primer%20bucket/images/sr_iso_white.png"
+              alt="isologo_elephant"
+            />
           </Link>
         </div>
       </div>
